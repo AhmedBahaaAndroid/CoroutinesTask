5ba959d03a103ce034ad9fbdfc162fadd8be4a78
EngAhmedBahaa <ahmed.bahaa@elmenus.com>
2021-10-27 00:07:00 +0200

    seprate viewmodel

--- app/src/main/java/com/example/freenowapp/ui/homeView/viewModel/VehiclesOnMapViewModel.kt
+++ app/src/main/java/com/example/freenowapp/ui/homeView/viewModel/VehiclesOnMapViewModel.kt
@@ -4,10 +4,6 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.freenowapp.DEF_P1LAT
-import com.example.freenowapp.DEF_P1LONG
-import com.example.freenowapp.DEF_P2LAT
-import com.example.freenowapp.DEF_P2LONG
 import com.example.freenowapp.domain.GetVehicles
 import com.example.freenowapp.extensions.handleError
 import com.example.freenowapp.ui.homeView.uiModel.VehicleUIModel
@@ -17,33 +13,24 @@ import com.example.freenowapp.utils.ViewState
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
-class VehiclesViewModel(private val getVehicles: GetVehicles) : ViewModel() {
+class VehiclesOnMapViewModel(private val getVehicles: GetVehicles) : ViewModel() {
 
     private val _viewState = MutableLiveData<ViewState>()
     val viewState: LiveData<ViewState>
         get() = _viewState
 
-    private val _vehicles = MutableLiveData<List<VehicleUIModel>>()
-    val vehicles: LiveData<List<VehicleUIModel>>
-        get() = _vehicles
+    private val _vehiclesInBounds = MutableLiveData<List<VehicleUIModel>>()
+    val vehiclesInBounds: LiveData<List<VehicleUIModel>>
+        get() = _vehiclesInBounds
 
-    private val _selectedVehicle = MutableLiveData<VehicleUIModel>()
-    val selectedVehicle: LiveData<VehicleUIModel>
-        get() = _selectedVehicle
-
-
-    init {
-        getVehicles()
-    }
-
-    private fun getVehicles() {
+    fun getVehiclesListInBounds(p1Lat: Double, p1Lon: Double, p2Lat: Double, p2Lon: Double) {
         viewModelScope.launch(Dispatchers.Main) {
             _viewState.value = ViewState.Loading
-            val response = getVehicles.execute(DEF_P1LAT, DEF_P1LONG, DEF_P2LAT, DEF_P2LONG)
+            val response = getVehicles.execute(p1Lat, p1Lon, p2Lat, p2Lon)
             when (response.status) {
                 Status.SUCCESS -> {
                     _viewState.value = ViewState.Success
-                    _vehicles.value = response.data?.map { it.toVehicleUIModel() }
+                    _vehiclesInBounds.value = response.data?.map { it.toVehicleUIModel() }
                 }
                 Status.ERROR -> _viewState.value =
                     ViewState.Error(response.appException?.handleError())
@@ -51,12 +38,4 @@ class VehiclesViewModel(private val getVehicles: GetVehicles) : ViewModel() {
         }
     }
 
-
-    fun onVehicleSelected(id: Int) {
-        _selectedVehicle.value = _vehicles.value?.find { it.id == id }
-    }
-
-    fun onRefreshData() {
-        getVehicles()
-    }
 }
